798dc2afe01220639132abc444d5fb8b112ddfc2
katarzynakaz <l453r3@gmail.com>
2026-02-17 18:32:20 +0000

    completed up to polling

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,16 +1,19 @@
 import express from "express";
 import cors from "cors";
-import { text } from "body-parser";
+import { on } from "events";
+// import { text } from "body-parser"; issue with this so replaced with express
 
 const app = express();
 const port = 3000;
 app.use(cors());
+// app.use(express.json());
 
-const quotes = [
+const messages = [
   {
-    "id": 1,
-    "username": "annonymous",
-    "text": "First chat message",
+    id: 1,
+    username: "annonymous",
+    msgText: "First chat message",
+    timestamp: Date.now()
   },
   
 ];
@@ -22,13 +25,54 @@ const quotes = [
 // }
 
 //view all messages
+//changing "/" to "/messages" and also added to get all messages on load of page in frontend
+// this was before polling
+// app.get("/", (req, res) => {
+//   // console.log("test");
+// //   const quote = randomQuote();
+//   res.json(messages);
+// });
+
+//now with added poling since last id
+// from fe
+// const keepFetchingMessages = async () => {
+//     const lastSeenId = messages.length > 0 ? messages[messages.length - 1].id : null;
+//     const queryString = lastSeenId ? `?since=${lastSeenId}` : "";
+//     const server = url
+//     const urlofserv = `${server}messages${queryString}`;
+//     const rawResponse = await fetch(urlofserv);
+//     const response = await rawResponse.json();
+//     messages.push(...response);
+//     // render();
+//     seeAllMessages();
+//     setTimeout(keepFetchingMessages, 100);
+// }
+
+  
+// });
+//get all messages
 app.get("/", (req, res) => {
-  // console.log("test");
-//   const quote = randomQuote();
-  res.json(messages);
+    
+    res.json(messages);
+  
+});
+//get recent messages
+app.get("/messages", (req, res) => {
+    //sincetimestapm Teach your backend how to answer “since when” queries.
+    const since = parseInt(req.query.since);
+    if (since) {
+        const onlyRecentMsgs = messages.filter(msg => msg.timestamp > since);
+        res.json(onlyRecentMsgs);
+        return;
+    }
+    //keep if no since and show all
+    res.json(messages);
+  
 });
 
+//add msg to chat
 app.post("/", (req, res) => {
+    
   const bodyBytes = [];
   req.on("data", chunk => bodyBytes.push(...chunk));
   req.on("end", () => {
@@ -41,9 +85,9 @@ app.post("/", (req, res) => {
       res.status(400).send("Expected body to be JSON.");
       return;
     }
-    if (typeof body != "object" || !("username" in body) || !("msgtext" in body)) {
-      console.error(`Failed to extract quote and author from post body: ${bodyString}`);
-      res.status(400).send("Expected body to be a JSON object containing keys quote and author.");
+    if (typeof body != "object" || !("username" in body) || !("msgText" in body)) {
+      console.error(`Failed to extract username and message text from body: ${bodyString}`);
+      res.status(400).send("Expected body to be a JSON object containing keys username and message text.");
       return;
     }
     //here add the checks on backedn 400
@@ -58,28 +102,32 @@ app.post("/", (req, res) => {
     //     confirmToUser.innerHTML = "Quote must be up to 250 chars and author must be less than 40 chars.";
     //     return;
     // }
-    body.quote = body.quote.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
-    body.author = body.author.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
+    body.msgText = body.msgText.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
+    body.username = body.username.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
 
-    // 2. Reuse your FE logic (checking body.quote instead of newQuoteText)
-    if (!body.quote || !body.author) {
-      res.status(400).send("Please add a quote and an author.");
+    if (!body.msgText || !body.username) {
+      res.status(400).send("Please add a quote and an username.");
       return;
     } 
 
-    if (body.quote.length > 250 || body.author.length > 40) {
-      res.status(400).send("Quote must be up to 250 chars and author must be less than 40 chars.");
+    if (body.msgText.length >400 || body.username.length > 40) {
+      res.status(400).send("Message text must be up to 400 chars and username must be less than 40 chars.");
       return;
     }
-    
-    quotes.push({
-      quote: body.quote,
-      author: body.author,
+
+    // to add new id to message
+    const newId = messages.length + 1;
+
+    messages.push({
+        id: newId,
+      msgText: body.msgText,
+      username: body.username,
+      timestamp: Date.now(),
     });
     res.send("ok");
   });
 });
 
 app.listen(port, () => {
-  console.error(`Quote server listening on port ${port}`);
+  console.error(`Chat server listening on port ${port}`);
 });
