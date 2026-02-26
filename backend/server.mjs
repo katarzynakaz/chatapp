2129903c98fa12da6048024f0f064ddcfbfaf925
katarzynakaz <l453r3@gmail.com>
2026-02-26 13:34:55 +0000

    node modules removed

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,39 +1,51 @@
+//imports
 import express from "express";
 import cors from "cors";
-import { on } from "events";
-// import { text } from "body-parser"; issue with this so replaced with express
 
 const app = express();
-// const port = 3000;
-// because server was not working
-const port = process.env.PORT || 3000;
+
+// following coursework
+import { server as WebSocketServer } from "websocket";
+
+import http from "http";
+
+const server = http.createServer(app);
+const webSocketServer = new WebSocketServer({ httpServer: server });
+
+const port = 3000;
 app.use(cors());
+
+webSocketServer.on("request", (request) => {
+	const connection = request.accept(null, request.origin);
+
+	connection.sendUTF("Hello from server");
+
+	connection.on("message", (message) => {
+		console.log("Msg from client", message.utf8Data);
+	});
+});
 
 const messages = [
 	{
 		id: 1,
-		username: "annonymous",
-		msgText: "First chat message",
+		username: "Kaska",
+		msgText: "Hey people, how are you doing?",
 		timestamp: Date.now(),
+		likesCount: 0,
+		dislikesCount: 0,
 	},
 ];
 
-//now with added poling since last id
-// from fe
-// const keepFetchingMessages = async () => {
-//     const lastSeenId = messages.length > 0 ? messages[messages.length - 1].id : null;
-//     const queryString = lastSeenId ? `?since=${lastSeenId}` : "";
-//     const server = url
-//     const urlofserv = `${server}messages${queryString}`;
-//     const rawResponse = await fetch(urlofserv);
-//     const response = await rawResponse.json();
-//     messages.push(...response);
-//     // render();
-//     seeAllMessages();
-//     setTimeout(keepFetchingMessages, 100);
-// }
+let globalIdCounter = 2;
 
-// });
+const getRecentMessages = (sinceTime) => {
+	const since = parseInt(sinceTime);
+	if (since) {
+		return messages.filter((msg) => msg.timestamp > since);
+	}
+	return messages;
+};
+
 //get all messages
 app.get("/", (req, res) => {
 	res.json(messages);
@@ -41,15 +53,7 @@ app.get("/", (req, res) => {
 
 //get recent messages poll
 app.get("/messages", (req, res) => {
-	//sincetimestapm Teach your backend how to answer “since when” queries.
-	const since = parseInt(req.query.since);
-	if (since) {
-		const onlyRecentMsgs = messages.filter((msg) => msg.timestamp > since);
-		res.json(onlyRecentMsgs);
-		return;
-	}
-	//keep if no since and show all
-	res.json(messages);
+	res.json(getRecentMessages(req.query.since));
 });
 
 //messages with long polling only
@@ -57,15 +61,10 @@ const callbacksForNewMessages = [];
 app.get("/long-poll", (req, res) => {
 	let messagesToSend = [];
 
-	//since from messages get
-	const since = parseInt(req.query.since);
-	if (since) {
-		messagesToSend = messages.filter((msg) => msg.timestamp > since);
+	if (req.query.since) {
+		messagesToSend = getRecentMessages(req.query.since);
 	}
 
-	//from coursework pasted
-	// Now, if 'since' was provided but no NEW messages were found,
-	// messagesToSend.length will be 0, and the server will WAIT.
 	if (messagesToSend.length === 0) {
 		callbacksForNewMessages.push((value) => res.send(value));
 	} else {
@@ -73,7 +72,6 @@ app.get("/long-poll", (req, res) => {
 	}
 });
 
-//add msg to chat
 app.post("/", (req, res) => {
 	const bodyBytes = [];
 	req.on("data", (chunk) => bodyBytes.push(...chunk));
@@ -102,7 +100,6 @@ app.post("/", (req, res) => {
 				);
 			return;
 		}
-		//here add the checks on backedn 400
 
 		body.msgText = body.msgText.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
 		body.username = body.username.trim().replace(/[^a-zA-Z0-9,.;:?! ]/g, "");
@@ -112,7 +109,7 @@ app.post("/", (req, res) => {
 			return;
 		}
 
-		if (body.msgText.length > 400 || body.username.length > 40) {
+		if (body.msgText.length > 400 || body.username.length >= 40) {
 			res
 				.status(400)
 				.send(
@@ -121,21 +118,16 @@ app.post("/", (req, res) => {
 			return;
 		}
 
-		// to add new id to message
-		const newId = messages.length + 1;
+		const newId = globalIdCounter++;
 
-		// messages.push({
-		//   id: newId,
-		//   msgText: body.msgText,
-		//   username: body.username,
-		//   timestamp: Date.now(),
-		// });
-		//newMessage obnj to send to long poll so instead of above
 		const newMessage = {
 			id: newId,
 			msgText: body.msgText,
 			username: body.username,
 			timestamp: Date.now(),
+			//updated initialised
+			likesCount: 0,
+			dislikesCount: 0,
 		};
 
 		messages.push(newMessage);
@@ -149,6 +141,70 @@ app.post("/", (req, res) => {
 	});
 });
 
-app.listen(port, () => {
-	console.error(`Chat server listening on port ${port}`);
+//add liking disliking route
+app.post("/vote", (req, res) => {
+	const bodyBytes = [];
+	req.on("data", (chunk) => bodyBytes.push(...chunk));
+	req.on("end", () => {
+		const bodyString = String.fromCharCode(...bodyBytes);
+		let body;
+		try {
+			body = JSON.parse(bodyString);
+		} catch (error) {
+			console.error(`Failed to parse body ${bodyString} as JSON: ${error}`);
+			res.status(400).send("Expected body to be JSON.");
+			return;
+		}
+		if (typeof body != "object" || !("id" in body) || !("vote" in body)) {
+			console.error(`Failed to extract id and vote type.`);
+			res
+				.status(400)
+				.send(
+					"Expected body to be a JSON object containing keys uis and vote type."
+				);
+			return;
+		}
+
+		//add like and dislike
+		const likeOrDislike = () => {
+			for (const message of messages) {
+				if (message.id === body.id) {
+					return message;
+				}
+			}
+			return null;
+		};
+		const currentyLikedDislikedMsg = likeOrDislike();
+
+		if (!currentyLikedDislikedMsg) {
+			res.status(404).send("No message with this id.");
+			return;
+		}
+
+		if (body.vote === "like") {
+			currentyLikedDislikedMsg.likesCount += 1;
+			currentyLikedDislikedMsg.timestamp = Date.now();
+		} else if (body.vote === "dislike") {
+			currentyLikedDislikedMsg.dislikesCount += 1;
+			currentyLikedDislikedMsg.timestamp = Date.now();
+		} else {
+			//here to add invalid vote type
+			res.status(400).send("Invalid vote type.");
+			return;
+		}
+
+		while (callbacksForNewMessages.length > 0) {
+			const callback = callbacksForNewMessages.pop();
+			callback([currentyLikedDislikedMsg]);
+		}
+
+		res.json(currentyLikedDislikedMsg);
+	});
+});
+
+// this was not working with websocket so the first line is changed to server listening
+// app.listen(port, () => {
+
+server.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
 });
